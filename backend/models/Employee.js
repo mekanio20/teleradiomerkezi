2fc347b9e56b45927098948701e070e13a6ed837
mekanio20 <mrxyok138@gmail.com>
2025-03-29 17:43:18 +0500

    first commit

---
+++ backend/models/Employee.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Employee = sequelize.define('Employee', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  birthDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  department: {
+    type: DataTypes.ENUM('television', 'radio', 'cinematography'),
+    allowNull: false
+  },
+  position: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  address: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  }
+});
+
+module.exports = Employee; 
